7522804b332abfd057420ab4d437ee8cc48fbafc
Bugoy <azarcon.carlangelo@gmail.com>
2025-05-09 16:50:10 +0800

    changed vertical chart to horizontal chart

--- index.js
+++ index.js
@@ -99,14 +99,42 @@ console.log(volumes);
 let barChart = new Chart(myChart, {
     type: 'bar',
     data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1
-      }]
-    },
+	  labels: topics,
+	  datasets: [{
+	    label: '# of tweets/xeets',
+	    data: volumes,
+	    borderWidth: 2,
+	    backgroundColor: [
+	        'rgba(255, 99, 132, 0.2)',
+	        'rgba(255, 159, 64, 0.2)',
+	        'rgba(255, 205, 86, 0.2)',
+	        'rgba(75, 192, 192, 0.2)',
+	        'rgba(54, 162, 235, 0.2)',
+	        'rgba(153, 102, 255, 0.2)',
+	        'rgba(201, 203, 207, 0.2)'
+	    ],
+	    borderColor: [
+	        'rgb(255, 99, 132)',
+	        'rgb(255, 159, 64)',
+	        'rgb(255, 205, 86)',
+	        'rgb(75, 192, 192)',
+	        'rgb(54, 162, 235)',
+	        'rgb(153, 102, 255)',
+	        'rgb(201, 203, 207)'
+	    ],
+	    hoverBackgroundColor: [
+	        'rgb(255, 99, 132)',
+	        'rgb(255, 159, 64)',
+	        'rgb(255, 205, 86)',
+	        'rgb(75, 192, 192)',
+	        'rgb(54, 162, 235)',
+	        'rgb(153, 102, 255)',
+	        'rgb(201, 203, 207)'
+	    ]
+	  }]
+	},
     options: {
+    indexAxis:'y',
       scales: {
         y: {
           beginAtZero: true
